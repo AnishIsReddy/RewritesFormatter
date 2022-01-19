7247d94c36dd0a8289aaf756dff123b7679a8cea
Anish Reddy <anishisreddy95@gmail.com>
2022-01-19 13:32:01 -0500

    Updated build file to automatically use the same chromedriver as the uncompiled code

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 pyinstaller -F -w --clean PsychRewritesFormatter.py
+copy chromedriver.exe dist\chromedriver.exe
 powershell Compress-Archive -Update dist dist.zip
 del /q PsychRewritesFormatter.spec 
 rmdir /s /q __pycache__ build
